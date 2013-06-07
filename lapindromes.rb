e53985a3cfb9e66ecb6266d8b714698523b64210
Daniele <byakugan.87@gmail.com>
2013-06-07 16:54:58 +0100

    first commit

---
+++ lapindromes.rb
@@ -0,0 +1,17 @@
+def lapindrome?(string)
+  len = string.length
+  if len.even?
+    s1 = string[0..(len-1)/2]
+    s2 = string[len/2..-1]
+  else
+    s1 = string[0..len/2-1]
+    s2 = string[(len/2)+1..-1]
+  end
+  if s1.chars.sort == s2.chars.sort
+    puts 'YES'
+  else
+    puts 'NO'
+  end
+end
+
+puts lapindrome? 'rotor'
